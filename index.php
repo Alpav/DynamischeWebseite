18c4fdd011f8da0cfa4ca2b0b84e77912134ab52
Alpav <43410440+Alpav@users.noreply.github.com>
2019-05-14 14:24:35 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -1,11 +1,23 @@
 <?php
-	if (!empty($_SERVER['HTTPS']) && ('on' == $_SERVER['HTTPS'])) {
-		$uri = 'https://';
-	} else {
-		$uri = 'http://';
-	}
-	$uri .= $_SERVER['HTTP_HOST'];
-	header('Location: '.$uri.'/dashboard/');
-	exit;
+  include_once 'includes/dbh.inc.php'
 ?>
-Something is wrong with the XAMPP installation :-(
+
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+
+  <title>FreshQuotes</title>
+  <meta name="description" content="Quotes">
+  <meta name="author" content="Pavlov">
+
+  <link rel="stylesheet" href="css/styles.css?v=1.0">
+</head>
+
+<body>
+  <?php
+    $conn;
+    $sql = "SELECT quote, author FROM citation;";
+    $result = mysqli_query($conn, $sql);
+
+  ?>
