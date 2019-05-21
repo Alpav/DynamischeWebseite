cf4ce80a1f48cc35ba42f7f8d9a1256ee2b7f39a
andreasignjatovic <43343670+andreasignjatovic@users.noreply.github.com>
2019-05-21 14:47:24 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -1,23 +1,53 @@
-<?php
-  include_once 'includes/dbh.inc.php'
-?>
-
 <!doctype html>
+
 <html lang="en">
 <head>
+  <link rel="stylesheet" href="style.css">
   <meta charset="utf-8">
 
-  <title>FreshQuotes</title>
-  <meta name="description" content="Quotes">
-  <meta name="author" content="Pavlov">
+  <title>Zitate</title>
 
-  <link rel="stylesheet" href="css/styles.css?v=1.0">
+
+
 </head>
 
 <body>
-  <?php
-    $conn;
-    $sql = "SELECT quote, author FROM citation;";
-    $result = mysqli_query($conn, $sql);
+  <h1 name="description" content="The HTML5 Herald">
+  <h2 name="author" content="SitePoint">
 
+  <?php
+
+  error_reporting(E_ALL);
+  define ( 'MYSQL_HOST',      'localhost' );
+  define ( 'MYSQL_BENUTZER',  'quotout' );
+  define ( 'MYSQL_KENNWORT',  'qu0t_' );
+  define ( 'MYSQL_DATENBANK', 'citation' );
+  $db_link = mysqli_connect (MYSQL_HOST,
+                             MYSQL_BENUTZER,
+                             MYSQL_KENNWORT,
+                             MYSQL_DATENBANK);
+  $sql = "SELECT quote,author FROM citation ORDER BY RAND() LIMIT 1";
+  $db_erg = mysqli_query( $db_link, $sql );
+  $quote = $db_erg->fetch_object();
+  if ( $db_link )
+  {
+  }
+  else
+  {
+      die('keine Verbindung möglich: ' . mysqli_error());
+  }
   ?>
+	  <h1>
+	    <?php
+	      echo $quote->quote;
+	      ?>
+	  </h1>
+
+	  <h2>
+	    <?php
+	      echo $quote->author;
+	    ?>
+	  </h2>
+	  <script src="js/scripts.js"></script>
+	</body>
+	</html>
